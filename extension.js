e9d3b210c63192a7e489b73825b6f61a41a52bcc
Mark Geck <mgeck64@gmail.com>
2021-10-19 21:45:57 -0700

    removed usage of deprecated Lang module

--- extension.js
+++ extension.js
@@ -1,10 +1,7 @@
-const St = imports.gi.St;
+const {St, GObject, Clutter, Gio} = imports.gi;
 const Main = imports.ui.main;
-const Lang = imports.lang;
-const Clutter = imports.gi.Clutter;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const Gio = imports.gi.Gio;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Mainloop = imports.mainloop;
 
@@ -333,202 +330,202 @@ class Parser {
 // Calculator-------------------------------------------------------------------
 // (calculator ui object)
 
-const Calculator = new Lang.Class({
-  Name: 'Calculator',
-  Extends: PanelMenu.Button,
+const Calculator = GObject.registerClass(
+  class Calculator extends PanelMenu.Button {
+    _init () {
+      super._init(0, 'Calculator', false);
 
-  _init () {
-    this.parent(0.0, 'Calculator', false);
-    this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.pcalc');
+      this._settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.pcalc');
 
-    // panel expression entry field
-    this._initEntry();
+      // panel expression entry field
+      this._initEntry();
 
-    // popup; will have secondary expression entry field and help content
-    this._initPopup();
+      // popup; will have secondary expression entry field and help content
+      this._initPopup();
 
-    // panel icon
-    // (do after settings bindings have been made)
-    this._initIcon();
+      // panel icon
+      // (do after settings bindings have been made)
+      this._initIcon();
 
-    // container (box) for entry and icon elements
-    this._initContainer();
+      // container (box) for entry and icon elements
+      this._initContainer();
 
-    // events
-    this._initEvents();
+      // events
+      this._initEvents();
 
-    // parser
-    this._parser = new Parser();
-  },
-
-  _initEntry () { // panel expression entry field
-    this._exprEntry = new St.Entry({
-      hint_text: 'Enter math expression',
-      track_hover: true,
-      can_focus: true,
-      y_align: Clutter.ActorAlign.CENTER,
-      style_class: 'expr-entry'
-    });
-
-    this._settings.bind(
-      'allow-entry-on-panel',
-      this._exprEntry,
-      'visible',
-      Gio.SettingsBindFlags.DEFAULT
-    );
-  },
-
-  _initPopup () { // popup will have secondary expression entry field and help content
-    const MENU_ITEM_WIDTH = 400;
-
-    this._exprEntry2 = new St.Entry({
-      hint_text: 'Enter a mathematical expression',
-      track_hover: true,
-      can_focus: true,
-      width: MENU_ITEM_WIDTH,
-      style_class: 'expr-entry2'
-    });
-    const menuItem = new PopupMenu.PopupBaseMenuItem({
-      reactive: false
-    });
-    menuItem.actor.add_actor(this._exprEntry2);
-    this.menu.addMenuItem(menuItem);
-
-    const helpText = new St.Label({
-      text: this._helpContent(),
-      width: MENU_ITEM_WIDTH,
-      style_class: 'help-text'
-    });
-    this._helpMenuItem = new PopupMenu.PopupBaseMenuItem({
-      reactive: false
-    });
-    this._helpMenuItem.actor.add_actor(helpText);
-    this.menu.addMenuItem(this._helpMenuItem);
-
-    this._settings.bind(
-      'show-help-on-popup',
-      this._helpMenuItem,
-      'visible',
-      Gio.SettingsBindFlags.DEFAULT
-    );
-  },
-
-  _initIcon () { // panel icon
-    this._icon = new St.Icon({
-      icon_name: this._iconName(),
-      y_align: Clutter.ActorAlign.CENTER,
-      style_class: 'system-status-icon'
-    });
-    this._settings.connect('changed', Lang.bind(this, this._settingsChanged));
-  },
-
-  _initContainer () { // container for entry and icon elements
-    const calcBox = new St.BoxLayout();
-    calcBox.add(this._exprEntry);
-    calcBox.add(this._icon);
-    this.actor.add_actor(calcBox);
-  },
-
-  _initEvents () { // events:
-    this.connect('button_press_event', Lang.bind(this, function (actor, event) {
-      this._exprEntry2.grab_key_focus();
-    }));
-
-    this._exprEntry.clutter_text.connect('key_focus_in', Lang.bind(this, this._on_key_focus_in));
-    this._exprEntry.clutter_text.connect('button-release-event', Lang.bind(this, this._on_button_release));
-    this._exprEntry.clutter_text.connect('activate', Lang.bind(this, this._on_activate));
-
-    this._exprEntry2.clutter_text.connect('key_focus_in', Lang.bind(this, this._on_key_focus_in));
-    this._exprEntry2.clutter_text.connect('button-release-event', Lang.bind(this, this._on_button_release));
-    this._exprEntry2.clutter_text.connect('activate', Lang.bind(this, this._on_activate));
-  },
-
-  _on_key_focus_in (actor, event) {
-    actor._exprFocusedIn = true;
-  },
-
-  _on_button_release (actor, event) {
-    if (actor._exprFocusedIn) {
-      actor.set_selection(0, actor.get_text().length); // doesn't work to simply do this in _on_key_focus_in; don't know why
-      actor._exprFocusedIn = false;
+      // parser
+      this._parser = new Parser();
     }
-  },
 
-  _on_activate (actor, event) {
-    let result;
-    try {
-      const expr = actor.get_text();
-      result = this._parser.eval(expr).toString();
-    } catch (e) {
-      if (e instanceof SyntaxError) {
-        result = 'Syntax error';
-      } else if (e instanceof UndefinedIdent) {
-        result = '"' + e.ident + '" is undefined';
-      } else if (e instanceof NoExpression) {
-        result = '';
-      } else {
-        result = 'Unexpected error';
+    _initEntry () { // panel expression entry field
+      this._exprEntry = new St.Entry({
+        hint_text: 'Enter math expression',
+        track_hover: true,
+        can_focus: true,
+        y_align: Clutter.ActorAlign.CENTER,
+        style_class: 'expr-entry'
+      });
+
+      this._settings.bind(
+        'allow-entry-on-panel',
+        this._exprEntry,
+        'visible',
+        Gio.SettingsBindFlags.DEFAULT
+      );
+    }
+
+    _initPopup () { // popup will have secondary expression entry field and help content
+      const MENU_ITEM_WIDTH = 400;
+
+      this._exprEntry2 = new St.Entry({
+        hint_text: 'Enter a mathematical expression',
+        track_hover: true,
+        can_focus: true,
+        width: MENU_ITEM_WIDTH,
+        style_class: 'expr-entry2'
+      });
+      const menuItem = new PopupMenu.PopupBaseMenuItem({
+        reactive: false
+      });
+      menuItem.actor.add_actor(this._exprEntry2);
+      this.menu.addMenuItem(menuItem);
+
+      const helpText = new St.Label({
+        text: this._helpContent(),
+        width: MENU_ITEM_WIDTH,
+        style_class: 'help-text'
+      });
+      this._helpMenuItem = new PopupMenu.PopupBaseMenuItem({
+        reactive: false
+      });
+      this._helpMenuItem.actor.add_actor(helpText);
+      this.menu.addMenuItem(this._helpMenuItem);
+
+      this._settings.bind(
+        'show-help-on-popup',
+        this._helpMenuItem,
+        'visible',
+        Gio.SettingsBindFlags.DEFAULT
+      );
+    }
+
+    _initIcon () { // panel icon
+      this._icon = new St.Icon({
+        icon_name: this._iconName(),
+        y_align: Clutter.ActorAlign.CENTER,
+        style_class: 'system-status-icon'
+      });
+      this._settings.connect('changed', this._settingsChanged.bind(this));
+    }
+
+    _initContainer () { // container for entry and icon elements
+      const calcBox = new St.BoxLayout();
+      calcBox.add(this._exprEntry);
+      calcBox.add(this._icon);
+      this.actor.add_actor(calcBox);
+    }
+
+    _initEvents () { // events:
+      this.connect('button_press_event', function (actor, event) {
+        this._exprEntry2.grab_key_focus();
+      }.bind(this));
+
+      this._exprEntry.clutter_text.connect('key_focus_in', this._onKeyFocusIn.bind(this));
+      this._exprEntry.clutter_text.connect('button-release-event', this._onButtonRelease.bind(this));
+      this._exprEntry.clutter_text.connect('activate', this._onActivate.bind(this));
+
+      this._exprEntry2.clutter_text.connect('key_focus_in', this._onKeyFocusIn.bind(this));
+      this._exprEntry2.clutter_text.connect('button-release-event', this._onButtonRelease.bind(this));
+      this._exprEntry2.clutter_text.connect('activate', this._onActivate.bind(this));
+    }
+
+    _onKeyFocusIn (actor, event) {
+      actor._exprFocusedIn = true;
+    }
+
+    _onButtonRelease (actor, event) {
+      if (actor._exprFocusedIn) {
+        actor.set_selection(0, actor.get_text().length); // doesn't work to simply do this in _onKeyFocusIn; don't know why
+        actor._exprFocusedIn = false;
       }
     }
-    this._exprEntry.set_text(result);
-    this._exprEntry2.set_text(result);
-    actor.set_selection(0, result.length);
-  },
 
-  _iconName () {
-    if (this._exprEntry.visible && this._helpMenuItem.visible) {
-      return 'dialog-question-symbolic';
+    _onActivate (actor, event) {
+      let result;
+      try {
+        const expr = actor.get_text();
+        result = this._parser.eval(expr).toString();
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          result = 'Syntax error';
+        } else if (e instanceof UndefinedIdent) {
+          result = '"' + e.ident + '" is undefined';
+        } else if (e instanceof NoExpression) {
+          result = '';
+        } else {
+          result = 'Unexpected error';
+        }
+      }
+      this._exprEntry.set_text(result);
+      this._exprEntry2.set_text(result);
+      actor.set_selection(0, result.length);
     }
-    return 'accessories-calculator-symbolic';
-  },
 
-  _settingsChanged () {
-    this._icon.icon_name = this._iconName();
-  },
+    _iconName () {
+      if (this._exprEntry.visible && this._helpMenuItem.visible) {
+        return 'dialog-question-symbolic';
+      }
+      return 'accessories-calculator-symbolic';
+    }
 
-  _helpContent () {
-    return '\
-An example of an expression is 2+4*8, which means multiply\n\
-4 by 8 and add 2 to the result. Supported operators:\n\
-    + addition\n\
-    - subtraction and negation\n\
-    * multiplication\n\
-    / division\n\
-    ^ or ** exponentiation (right-associative)\n\
-\n\
-Use parentheses to override operator precedence; e.g.,\n\
-(2+4)*8 means add 2 to 4 and multiply the result by 8.\n\
-\n\
-The following special values and functions are available:\n\
-    pi : Did you know that March 14 is Pi day?\n\
-    e : Euler\'s number\n\
-    last : the last calculated value\n\
-    abs(x) : absolute value of x\n\
-    acos(x) : arccosine of x, in radians\n\
-    acosh(x) : hyperbolic arccosine of x\n\
-    asin(x) : arcsine of x, in radians\n\
-    asinh(x) : hyperbolic arcsine of x\n\
-    atan(x) : arctangent of x between -pi and pi radians\n\
-    atanh(x) : hyperbolic arctangent of x\n\
-    cbrt(x) : cubic root of x\n\
-    ceil(x) : x rounded upwards to the nearest integer\n\
-    cos(x) : cosine of x (x is in radians)\n\
-    cosh(x) : hyperbolic cosine of x\n\
-    exp(x) : value of e raised to the power of x\n\
-    floor(x) : x rounded downwards to the nearest integer\n\
-    ln(x) or log(x) : natural logarithm (base e) of x\n\
-    random() : random number between 0 and 1\n\
-    round(x) : rounds x to the nearest integer\n\
-    sin(x) : sine of x (x is in radians)\n\
-    sinh(x) : hyperbolic sine of x\n\
-    sqrt(x) : square root of x\n\
-    tan(x) : tangent of an angle\n\
-    tanh(x) : hyperbolic tangent of a number\n\
-    trunc(x) : integer part of a number x\n\
-    tanh(x) : hyperbolic tangent of a number\n\
-    trunc(x) : integer part of a number x';
+    _settingsChanged () {
+      this._icon.icon_name = this._iconName();
+    }
+
+    _helpContent () {
+      return '\
+  An example of an expression is 2+4*8, which means multiply\n\
+  4 by 8 and add 2 to the result. Supported operators:\n\
+      + addition\n\
+      - subtraction and negation\n\
+      * multiplication\n\
+      / division\n\
+      ^ or ** exponentiation (right-associative)\n\
+  \n\
+  Use parentheses to override operator precedence; e.g.,\n\
+  (2+4)*8 means add 2 to 4 and multiply the result by 8.\n\
+  \n\
+  The following special values and functions are available:\n\
+      pi : Did you know that March 14 is Pi day?\n\
+      e : Euler\'s number\n\
+      last : the last calculated value\n\
+      abs(x) : absolute value of x\n\
+      acos(x) : arccosine of x, in radians\n\
+      acosh(x) : hyperbolic arccosine of x\n\
+      asin(x) : arcsine of x, in radians\n\
+      asinh(x) : hyperbolic arcsine of x\n\
+      atan(x) : arctangent of x between -pi and pi radians\n\
+      atanh(x) : hyperbolic arctangent of x\n\
+      cbrt(x) : cubic root of x\n\
+      ceil(x) : x rounded upwards to the nearest integer\n\
+      cos(x) : cosine of x (x is in radians)\n\
+      cosh(x) : hyperbolic cosine of x\n\
+      exp(x) : value of e raised to the power of x\n\
+      floor(x) : x rounded downwards to the nearest integer\n\
+      ln(x) or log(x) : natural logarithm (base e) of x\n\
+      random() : random number between 0 and 1\n\
+      round(x) : rounds x to the nearest integer\n\
+      sin(x) : sine of x (x is in radians)\n\
+      sinh(x) : hyperbolic sine of x\n\
+      sqrt(x) : square root of x\n\
+      tan(x) : tangent of an angle\n\
+      tanh(x) : hyperbolic tangent of a number\n\
+      trunc(x) : integer part of a number x\n\
+      tanh(x) : hyperbolic tangent of a number\n\
+      trunc(x) : integer part of a number x';
+    }
   }
-});
+);
 
 // main program-----------------------------------------------------------------
 
@@ -539,9 +536,12 @@ function init () {
 
 function enable () {
   calculator = new Calculator();
-  Main.panel.addToStatusArea('tpcalc-indicator', calculator);
+  Main.panel.addToStatusArea('pcalc', calculator);
 }
 
 function disable () {
-  calculator.destroy();
+  if (calculator) {
+    calculator.destroy();
+    calculator = null;
+  }
 }
