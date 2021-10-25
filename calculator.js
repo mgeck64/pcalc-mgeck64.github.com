8560a464b84c57eef1b7914bfe8d4b7dd3d179c1
Mark Geck <mgeck64@gmail.com>
2021-10-24 19:59:36 -0700

    split up longish help content into 3 subparts

--- calculator.js
+++ calculator.js
@@ -9,6 +9,8 @@ const {NoExpression, UndefinedIdent, CantConvertNumber, Parser} = Me.imports.par
 // Calculator-------------------------------------------------------------------
 // (calculator user interface)
 
+const MENU_ITEM_WIDTH = 400;
+
 class _Calculator extends PanelMenu.Button {
   _init () {
     super._init(0, 'Calculator', false);
@@ -53,8 +55,6 @@ class _Calculator extends PanelMenu.Button {
   }
 
   _initPopup () { // popup will have secondary expression entry field and help content
-    const MENU_ITEM_WIDTH = 400;
-
     this._exprEntry2 = new St.Entry({
       hint_text: 'Enter a mathematical expression',
       track_hover: true,
@@ -68,20 +68,27 @@ class _Calculator extends PanelMenu.Button {
     menuItem.actor.add_actor(this._exprEntry2);
     this.menu.addMenuItem(menuItem);
 
-    const helpText = new St.Label({
-      text: this._helpContent(),
+    this._addSubmenuHelp('General Help', this._generalHelpText)
+    this._addSubmenuHelp('Special Values and Functions, Part 1', this._specialValuesAndFunctionsText1)
+    this._addSubmenuHelp('Special Values and Functions, Part 2', this._specialValuesAndFunctionsText2)
+  }
+
+  _addSubmenuHelp(title, helpTextFn) {
+    const label = new St.Label({
+      text: helpTextFn(),
       width: MENU_ITEM_WIDTH,
-      style_class: 'help-text'
     });
-    this._helpMenuItem = new PopupMenu.PopupBaseMenuItem({
+    const subMenuBaseItem = new PopupMenu.PopupBaseMenuItem({
       reactive: false
     });
-    this._helpMenuItem.actor.add_actor(helpText);
-    this.menu.addMenuItem(this._helpMenuItem);
+    subMenuBaseItem.actor.add_actor(label);
+    const subMenuItem = new PopupMenu.PopupSubMenuMenuItem(title, true);
+    subMenuItem.menu.addMenuItem(subMenuBaseItem);
+    this.menu.addMenuItem(subMenuItem);
 
     this._settings.bind(
       'show-help-on-popup',
-      this._helpMenuItem,
+      subMenuItem,
       'visible',
       Gio.SettingsBindFlags.DEFAULT
     );
@@ -104,29 +111,28 @@ class _Calculator extends PanelMenu.Button {
   }
 
   _initEvents () { // events:
-    this.connect('button_press_event', this._onButtonPressThis.bind(this));
+    this.connect('button-press-event', function () {
+      if (this.menu.isOpen) {
+        this._exprEntry2.grab_key_focus();
+      }
+    }.bind(this));
 
     this._exprEntry.clutter_text.connect('button-release-event', this._onButtonReleaseEntry.bind(this));
     this._exprEntry.clutter_text.connect('activate', this._onActivateEntry.bind(this));
 
     this._exprEntry2.clutter_text.connect('button-release-event', this._onButtonReleaseEntry.bind(this));
     this._exprEntry2.clutter_text.connect('activate', this._onActivateEntry.bind(this));
-    this._exprEntry2.clutter_text.connect('key_focus_out', this._onKeyFocusOutEntry.bind(this));
-  }
 
-  _onButtonPressThis () {
-    this._exprEntry2.grab_key_focus();
+    this._exprEntry2.clutter_text.connect('key_focus_out', function () { // (not necessary for _exprEntry)
+      const endIdx = actor.get_text().length;
+      actor.set_selection(endIdx, endIdx); // clear selection, put cursor at end so it will be there when re-focused
+    }.bind(this));
   }
 
   _onButtonReleaseEntry (actor, event) {
     if (actor.get_cursor_position() !== -1 && actor.get_selection().length === 0) {
       actor.set_selection(0, actor.get_text().length); // doesn't work to do this on button-press-event or key_focus_in; don't know why
     }
-  }
-
-  _onKeyFocusOutEntry (actor, event) { // (not necessary for _exprEntry)
-    const endIdx = actor.get_text().length;
-    actor.set_selection(endIdx, endIdx); // clear selection, put cursor at end so it will be there when re-focused
   }
 
   _onActivateEntry (actor, event) {
@@ -152,7 +158,7 @@ class _Calculator extends PanelMenu.Button {
   }
 
   _iconName () {
-    if (this._exprEntry.visible && this._helpMenuItem.visible) {
+    if (this._exprEntry.visible && this._settings.get_boolean('show-help-on-popup')) {
       return 'dialog-question-symbolic';
     }
     return 'accessories-calculator-symbolic';
@@ -162,10 +168,12 @@ class _Calculator extends PanelMenu.Button {
     this._icon.icon_name = this._iconName();
   }
 
-  _helpContent () {
+  _generalHelpText () {
     return '\
 An example of an expression is 2+4*8, which means multiply\n\
-4 by 8 and add 2 to the result. Supported operators:\n\
+4 by 8 and add 2 to the result.\n\
+\n\
+Supported operators:\n\
     + addition\n\
     - subtraction and negation\n\
     * multiplication\n\
@@ -175,33 +183,43 @@ An example of an expression is 2+4*8, which means multiply\n\
 Use parentheses to override operator precedence; e.g.,\n\
 (2+4)*8 means add 2 to 4 and multiply the result by 8.\n\
 \n\
-The following special values and functions are available:\n\
-    pi : Did you know that March 14 is Pi day?\n\
-    e : Euler\'s number\n\
-    last : The last calculated value\n\
-    abs(x) : Absolute value of x\n\
-    acos(x) : Arccosine of x, in radians\n\
-    acosh(x) : Hyperbolic arccosine of x\n\
-    asin(x) : Arcsine of x, in radians\n\
-    asinh(x) : Hyperbolic arcsine of x\n\
-    atan(x) : Arctangent of x between -pi and pi radians\n\
-    atan2(y, x) : Arctangent of the quotient of its arguments\n\
-    atanh(x) : Hyperbolic arctangent of x\n\
-    cbrt(x) : Cubic root of x\n\
-    ceil(x) : x rounded upwards to the nearest integer\n\
-    cos(x) : Cosine of x (x is in radians)\n\
-    cosh(x) : Hyperbolic cosine of x\n\
-    exp(x) : Value of e raised to the power of x\n\
-    floor(x) : x rounded downwards to the nearest integer\n\
-    ln(x) or log(x) : Natural logarithm (base e) of x\n\
-    random() : Random number between 0 and 1\n\
-    round(x) : Rounds x to the nearest integer\n\
-    sin(x) : Sine of x (x is in radians)\n\
-    sinh(x) : Hyperbolic sine of x\n\
-    sqrt(x) : Square root of x\n\
-    tan(x) : Tangent of an angle\n\
-    tanh(x) : Hyperbolic tangent of a number\n\
-    trunc(x) : Integer part of a number x';
+Numbers can have the 0b, 0o or 0x prefix, or can be\n\
+specified in exponentiation notation; e.g., 0b11011001,\n\
+0o331, 0xd9 and 2.17e+2 all specify the number 217.';
+  }
+
+  _specialValuesAndFunctionsText1 () {
+    return '\
+pi : Did you know that March 14 is Pi day?\n\
+e : Euler\'s number\n\
+last : The last calculated value\n\
+abs(x) : Absolute value of x\n\
+acos(x) : Arccosine of x, in radians\n\
+acosh(x) : Hyperbolic arccosine of x\n\
+asin(x) : Arcsine of x, in radians\n\
+asinh(x) : Hyperbolic arcsine of x\n\
+atan(x) : Arctangent of x between -pi and pi radians\n\
+atan2(y, x) : Arctangent of the quotient of its arguments\n\
+atanh(x) : Hyperbolic arctangent of x\n\
+cbrt(x) : Cubic root of x\n\
+ceil(x) : x rounded upwards to the nearest integer';
+  }
+
+  _specialValuesAndFunctionsText2 () {
+    return '\
+cos(x) : Cosine of x (x is in radians)\n\
+cosh(x) : Hyperbolic cosine of x\n\
+exp(x) : Value of e raised to the power of x\n\
+floor(x) : x rounded downwards to the nearest integer\n\
+ln(x) or log(x) : Natural logarithm (base e) of x\n\
+random() : Random number between 0 and 1\n\
+round(x) : Rounds x to the nearest integer\n\
+sin(x) : Sine of x (x is in radians)\n\
+sinh(x) : Hyperbolic sine of x\n\
+sqrt(x) : Square root of x\n\
+tan(x) : Tangent of an angle\n\
+tanh(x) : Hyperbolic tangent of a number\n\
+trunc(x) : Integer part of a number x';
   }
 }
 
